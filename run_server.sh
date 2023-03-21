b4a6412576d10bcccf800e8053ff149de3c23d62
GlennMey <32812151+GlennMey@users.noreply.github.com>
2023-03-21 13:09:40 +0100

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -4,4 +4,4 @@
 /usr/games/steamcmd +login anonymous +force_install_dir /server +app_update $STEAM_APP_ID validate +quit
 
 # Run server
-/server/DayZServer -dologs -adminlog -netlog -freezecheck -profiles="${PROFILE_DIR}" -config="${CONFIG_FILE}" -limitfps=999
+/server/DayZServer -nologs -profiles="${PROFILE_DIR}" -config="${CONFIG_FILE}" -limitfps=999
